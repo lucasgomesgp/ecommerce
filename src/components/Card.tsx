c1dff062a3ba3b4b2e42031d79d482097126a303
Lucas Gomes <lucasgomesbt@gmail.com>
2023-12-19 16:09:01 -0300

    fix: add favorite button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { currencyFormatter } from "@/utils/functions/currencyFormatter";
+import { HeartIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,8 +12,11 @@ interface Props {
 }
 export function Card({ id, title, image, subTitle, price }: Props) {
   return (
-    <Link href={`/product/${id}`} className="w-[282px]">
+    <Link href={`/product/${id}`} className="w-[282px] relative">
       <Image src={image} alt={title} width={282} height={370} quality={100} className="rounded-xl mb-[30px]"/>
+      <button className="absolute top-[26px] right-5 flex items-center justify-center rounded-full bg-white w-8 h-8">
+        <HeartIcon width={20} height={20} color="#3C4242"/>
+      </button>
       <div className="flex justify-between items-center">
         <div className="flex flex-col gap-1">
           <p className="font-bold overflow-hidden whitespace-nowrap text-ellipsis w-[152px]">{title}</p>
