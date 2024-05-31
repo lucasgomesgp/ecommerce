345f5eeff033d6ff495d04286a867a2288ce7f29
Lucas Gomes <lucasgomesbt@gmail.com>
2024-05-31 00:40:20 -0300

    fix: change dynamic page

--- src/app/user/orders/[id]/page.tsx
+++ src/app/user/orders/[id]/page.tsx
@@ -1,18 +1,18 @@
-import { ArrowLeft } from "@phosphor-icons/react";
+import { ArrowLeft } from "@/svgs/arrow-left";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { MainSideBarContent } from "@/components/MainSideBarContent";
 import { PathPage } from "@/components/PathPage";
 
-export default async function Page({ params }: { params: { itemId: string } }) {
+export default function Page({ params }: { params: number }) {
     return (
         <main className="flex flex-col overflow-hidden">
             <Header />
             <PathPage title="Orders" />
             <MainSideBarContent>
-                <div className="flex flex-[0.75] flex-col">
-                    <h1 className="font-coreSans text-[28px] font-semibold">Order Details</h1>
+                <div className="flex flex-[0.75] gap-3 items-center">
                     <ArrowLeft />
+                    <h1 className="font-coreSans text-[28px] font-semibold">Order Details</h1>
                 </div>
             </MainSideBarContent>
             <Footer />
