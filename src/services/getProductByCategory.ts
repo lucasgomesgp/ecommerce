858bc7c668fc9cc8b85e4f41bd7fbab02e392ad0
Lucas Gomes <lucasgomesbt@gmail.com>
2024-08-31 13:58:04 -0300

    fix: change responsability to fetch data for service getProductCategory

--- src/services/getProductByCategory.ts
+++ src/services/getProductByCategory.ts
@@ -1,4 +1,5 @@
 import { STRAPI_API_URL, STRAPI_TOKEN } from "@/utils/constants/strapi";
+
 import { IProduct } from "@/utils/types/IProducts";
 
 interface ProductCategory {
@@ -9,15 +10,19 @@ interface PromiseResponseData {
   data: IProduct[];
 }
 
-export async function getProductByCategory({category}:ProductCategory) : Promise<PromiseResponseData> {
-  const urlProduct = category === "men"? `${process.env.NEXT_PUBLIC_API_URL}/men` : `${process.env.NEXT_PUBLIC_API_URL}/women`;
-  const res = await fetch(urlProduct,
-    {
-      cache: "no-cache",
-      headers: {
-        Authorization: `Bearer ${STRAPI_TOKEN}`,
-      },
-    }
-  );
-  return res.json();
+export async function getProductByCategory({ category }: ProductCategory): Promise<PromiseResponseData> {
+  const urlProduct = category === "men" ? `${process.env.STRAPI_API_URL}/products?populate=*` : `${process.env.STRAPI_API_URL}/products?filters[category][$containsi]=women&populate=*`;
+  try {
+    const res = await fetch(urlProduct,
+      {
+        cache: "no-cache",
+        headers: {
+          Authorization: `Bearer ${STRAPI_TOKEN}`,
+        },
+      }
+    );
+    return res.json();
+  } catch {
+    throw new Error("Error on search products for " + category);
+  }
 }
