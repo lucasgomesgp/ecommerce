b7e236331b367656e05195a140ebf073254ffa65
Lucas Gomes <lucasgomesbt@gmail.com>
2025-07-20 17:22:35 -0300

    fix: change orders button signout bug

--- src/components/ButtonActionSideMenu.tsx
+++ src/components/ButtonActionSideMenu.tsx
@@ -2,27 +2,38 @@ import { signOut, useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> { title: string, destination?: string, children: ReactNode };
-export function ButtonActionSideMenu({ title, destination, children, ...rest }: Props) {
-    const pathname = usePathname();
-    const {data: session} = useSession();
-    const route = useRouter();
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  destination?: string;
+  children: ReactNode;
+}
+export function ButtonActionSideMenu({
+  title,
+  destination,
+  children,
+  ...rest
+}: Props) {
+  const pathname = usePathname();
+  const { data: session } = useSession();
+  const route = useRouter();
 
-    function handleRedirectOrSignOut(){
-        if(session?.user?.email){
-            signOut();
-        }
-        if(destination){
-            route.push(destination);
-        }
+  function handleRedirect() {
+    if (destination) {
+      route.push(destination);
     }
-    return (
-        <button
-         className={`flex items-center justify-start gap-[15px] hover:opacity-80 transition-opacity py-[11px] max-w-[295px] w-[295px] cursor-pointer  disabled:opacity-70 disabled:cursor-not-allowed pl-[37px] rounded-tr-lg rounded-br-lg hover:scale-110 ${destination === pathname ? "bg-white-light border-l-2 border-l-gray-text-menu":""}`} {...rest}
-        onClick={handleRedirectOrSignOut}
-         >
-            {children}
-            <p className="text-gray-light text-lg font-semibold">{title}</p>
-        </button>
-    )
+  }
+  return (
+    <button
+      className={`flex items-center justify-start gap-[15px] hover:opacity-80 transition-opacity py-[11px] max-w-[295px] w-[295px] cursor-pointer  disabled:opacity-70 disabled:cursor-not-allowed pl-[37px] rounded-tr-lg rounded-br-lg hover:scale-110 ${
+        destination === pathname
+          ? "bg-white-light border-l-2 border-l-gray-text-menu"
+          : ""
+      }`}
+      {...rest}
+      onClick={handleRedirect}
+    >
+      {children}
+      <p className="text-gray-light text-lg font-semibold">{title}</p>
+    </button>
+  );
 }
--- src/components/SideMenuUser.tsx
+++ src/components/SideMenuUser.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { signOut, useSession } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react";
 
 import { ButtonActionSideMenu } from "./ButtonActionSideMenu";
 import { InfoUser } from "@/svgs/info-user";
@@ -9,35 +9,49 @@ import { SignOut } from "@/svgs/sign-out";
 import { Wishlist } from "@/svgs/wishlist";
 
 export function SideMenuUser() {
-    const { data: session } = useSession();
-    const isLoggedIn = session?.user?.email;
+  const { data: session } = useSession();
+  const isLoggedIn = session?.user?.email;
 
-    return (
-        <>
-            {isLoggedIn && (
-                <section className="flex flex-col gap-10">
-                    <div className="flex flex-col items-start justify-center">
-                        <h3 className="font-medium font-coreSans text-[28px] pl-[15px] text-gray-text-menu relative before:absolute before:w-[6px] before:h-7 before:bg-purple-principal before:rounded-[10px] before:top-0 before:bottom-0 before:left-0 before:m-auto">
-                            Hello {session?.user?.name || ""}
-                        </h3>
-                        <p className="text-gray-light text-sm">Welcome to your Account</p>
-                    </div>
-                    <section className="flex flex-col">
-                        <ButtonActionSideMenu title="My Orders" destination="/user/orders">
-                            <Orders />
-                        </ButtonActionSideMenu>
-                        <ButtonActionSideMenu title="Wishlist" destination="/user/wishlist">
-                            <Wishlist />
-                        </ButtonActionSideMenu>
-                        <ButtonActionSideMenu title="My info" disabled={!isLoggedIn} destination="/user/info">
-                            <InfoUser />
-                        </ButtonActionSideMenu>
-                        <ButtonActionSideMenu title="Sign out" disabled={!isLoggedIn} onClick={() => { signOut() }}>
-                            <SignOut />
-                        </ButtonActionSideMenu>
-                    </section>
-                </section>
-            )}
-        </>
-    );
+  return (
+    <>
+      {isLoggedIn && (
+        <section className="flex flex-col gap-10">
+          <div className="flex flex-col items-start justify-center">
+            <h3 className="font-medium font-coreSans text-[28px] pl-[15px] text-gray-text-menu relative before:absolute before:w-[6px] before:h-7 before:bg-purple-principal before:rounded-[10px] before:top-0 before:bottom-0 before:left-0 before:m-auto">
+              Hello {session?.user?.name || ""}
+            </h3>
+            <p className="text-gray-light text-sm">Welcome to your Account</p>
+          </div>
+          <section className="flex flex-col">
+            <ButtonActionSideMenu
+              title="My Orders"
+              disabled={!isLoggedIn}
+              destination="/user/orders"
+            >
+              <Orders />
+            </ButtonActionSideMenu>
+            <ButtonActionSideMenu title="Wishlist" destination="/user/wishlist">
+              <Wishlist />
+            </ButtonActionSideMenu>
+            <ButtonActionSideMenu
+              title="My info"
+              disabled={!isLoggedIn}
+              destination="/user/info"
+            >
+              <InfoUser />
+            </ButtonActionSideMenu>
+            <ButtonActionSideMenu
+              title="Sign out"
+              disabled={!isLoggedIn}
+              onClick={() => {
+                signOut();
+              }}
+            >
+              <SignOut />
+            </ButtonActionSideMenu>
+          </section>
+        </section>
+      )}
+    </>
+  );
 }
